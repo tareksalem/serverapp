7c99081bc05cd257d813ecab63d78a9b494b2ef9
tareksalem <tarekpopp@gmail.com>
2018-01-10 11:07:38 +0200

    l

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     var video = $(".container-cover video");
     var videoJs = document.querySelector("video");
     var containerVideo = $(".container-cover");
+    videoJs.addEventListener("click", function () {
+        videoJs.play();
+    }); 
     $(video).animate({"top": "20px"}, 1000, function () {
         this.click();
-        videoJs.addEventListener("click", function () {
-            videoJs.play();
-        });
     });
 });
