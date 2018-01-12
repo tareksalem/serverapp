cc5d4848c27cfddc5d34dd13cb09efd8d1a526db
tareksalem <tarekpopp@gmail.com>
2018-01-12 14:39:35 +0200

    a

--- backend/functions.js
+++ backend/functions.js
@@ -2,46 +2,9 @@ const http = require("http");
 const path = require("path");
 const url = require("url");
 const fs = require("fs");
-var staticFolders = require("../config");
+var server = require("../server");
 //functions
 
+//var handle = new SampleJs();
 
-var handle = {
-    get: function (req, res, link, file) {
-        if (req.method == "GET") {
-            var filename = staticFolders.htmlView + file;
-            fs.readFile(filename, "UTF-8", function (err, html) {
-                res.writeHead(200, {"content-type": "text/html"});
-                res.end(html);
-            });
-            if (req.url.match(/.css$/)) {
-                var cssSource = staticFolders.public + req.url;
-                var cssStream = fs.createReadStream(cssSource, "UTF-8");
-                res.writeHead(200, {"content-type": "text/css"});
-                cssStream.pipe(res);
-            } else if (req.url.match(/.js$/)) {
-                var jsSource = staticFolders.public + req.url;
-                var jsStream = fs.createReadStream(jsSource, "UTF-8");
-                res.writeHead(200, {"content-type": "text/js"});
-                jsStream.pipe(res);
-            } else if (req.url.match(/.jpg$/)) {
-                var jpgSource = staticFolders.public + req.url;
-                var jpgStream = fs.createReadStream(jpgSource, "UTF-8");
-                res.writeHead(200, {"content-type": "image/jpg"});
-                jpgStream.pipe(res);
-            } else if (req.url.match(/.png$/)) {
-                var pngSource = staticFolders.public + req.url;
-                var pngStream = fs.createReadStream(pngSource, "UTF-8");
-                res.writeHead(200, {"content-type": "image/png"});
-                pngStream.pipe(res);
-            } else if (req.url.match(/.jpeg$/)) {
-                var jpegSource = staticFolders.public + req.url;
-                var jpegStream = fs.createReadStream(jpegSource, "UTF-8");
-                res.writeHead(200, {"content-type": "image/jpeg"});
-                jpegStream.pipe(res);
-            }
-
-        }
-    }
-};
-module.exports = handle;
+//module.exports = SampleJs();
--- server.js
+++ server.js
@@ -3,11 +3,70 @@ var fs = require("fs");
 var path = require("path");
 var url = require("url");
 var port = process.env.PORT || 3000;
-var handle = require("./backend/functions");
-
-var server = http.createServer(function (req, res) {
-    handle.get(req, res, "/", "home.html");
-});
-
-//console.log(handle.get("/", "home.html"))
+var staticFolders = require("./config");
+var server = http.createServer();
 server.listen(port);
+
+
+function SampleJs() {
+    this.get = function (pass, cb) {
+        server.on("request", function (request, response) {
+            var getUrl = url.parse(request.url).pathname;
+            var req = request;
+            var res = response;
+            res.render = function (pathFile, file, data) {
+                if (pass == getUrl) {
+                    var htmlSource = path.join(__dirname, pathFile, file);
+                    fs.readFile(htmlSource, function (err, html) {
+                        res.writeHead(200, {"content-type": "text/html"});
+                        res.write(html);
+                        res.end();
+                    });
+                }
+                if (req.url.match(/.css$/)) {
+                    var cssSource = path.join(staticFolders.public, req.url);
+                    var cssStream = fs.createReadStream(cssSource);
+                    res.writeHead(200, {"content-type": "text/css"});
+                    cssStream.pipe(res);
+                }
+                if (req.url.match(/.js$/)) {
+                    var jsSource = path.join(staticFolders.public, req.url);
+                    var jsStream = fs.createReadStream(jsSource);
+                    res.writeHead(200, {"content-type": "text/js"});
+                    jsStream.pipe(res);
+                }
+                if (req.url.match(/.jpg$/)) {
+                    var jpgSource = path.join(staticFolders.public, req.url);
+                    var jpgStream = fs.createReadStream(jpgSource);
+                    res.writeHead(200, {"content-type": "image/jpg"});
+                    jpgStream.pipe(res);
+                }
+                if (req.url.match(/.png$/)) {
+                    var pngSource = path.join(staticFolders.public, req.url);
+                    var pngStream = fs.createReadStream(pngSource);
+                    res.writeHead(200, {"content-type": "image/png"});
+                    pngStream.pipe(res);
+                }
+                if (req.url.match(/.jpeg$/)) {
+                    var jpegSource = path.join(staticFolders.public, req.url);
+                    var jpegStream = fs.createReadStream(jpegSource);
+                    res.writeHead(200, {"content-type": "image/jpeg"});
+                    jpegStream.pipe(res);
+                }
+                
+                console.log(req.url);
+            };
+            cb(req, res);
+        });
+    }
+}
+
+
+var Handle = new SampleJs();
+
+Handle.get("/", function (req, res) {
+    res.render("pages", "home.html", "hello");
+});
+Handle.get("/profile", function (req, res) {
+    res.render("pages", "profile.html", "hello");
+});
